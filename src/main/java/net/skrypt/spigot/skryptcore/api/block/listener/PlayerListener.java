85e162ba3858fd974b4f126d91f28d7e5c46b607
Lukas Frey <lukas.frey.cz@gmail.com>
2017-07-09 19:06:20 +0200

    Fixed NullPointerException error in PlayerInteractEvent

--- src/main/java/net/skrypt/spigot/skryptcore/api/block/listener/PlayerListener.java
+++ src/main/java/net/skrypt/spigot/skryptcore/api/block/listener/PlayerListener.java
@@ -1,6 +1,7 @@
 package net.skrypt.spigot.skryptcore.api.block.listener;
 
 import net.skrypt.spigot.skryptcore.api.block.CustomBlock;
+import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,6 +15,12 @@ public class PlayerListener implements Listener {
 	@EventHandler
 	public void onPlayerInteract(PlayerInteractEvent event) {
 		Block block = event.getClickedBlock();
+		
+		if (block == null)
+			return;
+		
+		if (block.getType() == Material.AIR)
+			return;
 		
 		// If placedBlocks doesn't cointain this block, the block is not custom.
 		if (!CustomBlock.placedBlocks.containsKey(block.getLocation()))
