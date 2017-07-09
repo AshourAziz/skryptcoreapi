6600d8ab65899f6818a156f27ed5ac299ef36ff0
Lukas Frey <lukas.frey.cz@gmail.com>
2017-07-09 11:46:57 +0200

    - NullPointerError fixed on block placement.

--- src/main/java/net/skrypt/spigot/skryptcore/api/block/listener/BlockListener.java
+++ src/main/java/net/skrypt/spigot/skryptcore/api/block/listener/BlockListener.java
@@ -1,12 +1,20 @@
 package net.skrypt.spigot.skryptcore.api.block.listener;
 
 import net.skrypt.spigot.skryptcore.api.block.CustomBlock;
+import net.skrypt.spigot.skryptcore.api.block.CustomBlockAPI;
+import net.skrypt.spigot.skryptcore.api.exceptions.VersionException;
+import net.skrypt.spigot.skryptcore.api.itemstack.CustomItemStack;
+import net.skrypt.spigot.skryptcore.api.itemstack.CustomItemStackAPI;
+import net.skrypt.spigot.skryptcore.api.nbt.NBTItemStack;
+import net.skrypt.spigot.skryptcore.api.nbt.exceptions.NBTException;
+import org.bukkit.Bukkit;
 import org.bukkit.block.Block;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.Plugin;
 
 /**
  * @author Skrypt
@@ -17,11 +25,31 @@ public class BlockListener implements Listener {
 	public void onBlockPlace(BlockPlaceEvent event) {
 		ItemStack itemStack = event.getItemInHand();
 		
-		if (!CustomBlock.blocks.containsKey(itemStack))
+		try {
+			NBTItemStack nbt = NBTItemStack.fromBukkit(itemStack);
+			if (nbt.hasNBT() && nbt.getBoolean("customItemStack")) {
+				Plugin plugin = Bukkit.getPluginManager().getPlugin(nbt.getString("plugin"));
+				try {
+					if (!CustomBlock.blocks.containsKey(Class.forName(nbt.getString("class"))))
+						return;
+					
+					CustomBlock customBlock = CustomBlock.blocks.get(Class.forName(nbt.getString("class")));
+					customBlock.onPlaceDefault(event);
+				} catch (ClassNotFoundException e) {
+					e.printStackTrace();
+				}
+			}
+		} catch (VersionException e) {
+			e.printUserFriendlyMessage();
+		} catch (NBTException e) {
+			e.printUserFriendlyMessage();
+		}
+		
+		/*if (!CustomBlock.blocks.containsKey(itemStack))
 			return;
 		
 		CustomBlock customBlock = CustomBlock.blocks.get(itemStack);
-		customBlock.onPlaceDefault(event);
+		customBlock.onPlaceDefault(event);*/
 	}
 	
 	@EventHandler
